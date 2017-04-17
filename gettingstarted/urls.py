c8e1a2985d5ef01105067b490bd5043fde6ef591
amyible <amy.liu@princeton.edu>
2017-04-17 14:36:39 -0400

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -15,12 +15,10 @@ urlpatterns = [
     url(r'accounts/login/$', django_cas_ng.views.login),
     url(r'accounts/logout/$', django_cas_ng.views.logout),
     url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^db$', hello.views.db, name='db'),
     url(r'^about$', hello.views.about,name='about'),
-    #url(r'^meet/$', hello.views.meet,name='meet'),
-    #url(r'^casCGI/$', hello.views.casCGI,name='casCGI')
-    #url(r'^meet', hello.views.about),
-    #url(r'^casCGI', hello.views.casCGI),
-    #url(r'statussite', docs.views.statussite),
+    url(r'^meet$', hello.views.meet,name='meet'),
+    url(r'^casCGI$', hello.views.casCGI,name='casCGI')
+    url(r'^statussite$', docs.views.statussite, name='statussite'),
     url(r'^admin/$', include(admin.site.urls)),
 ]
