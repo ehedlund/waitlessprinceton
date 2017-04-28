2fd1d309c057cb22a7c9076f4eb7054942131214
Emily Hedlund <ehedlund@princeton.edu>
2017-04-28 16:58:06 -0400

    Add files via upload

--- hello/admin.py
+++ hello/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Building, BuildingInstance
-
-admin.site.register(Building)
-admin.site.register(BuildingInstance)
