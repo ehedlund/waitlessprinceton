b8ae1e8f4b4f478625302e58455e2919c1f78d66
Emily Hedlund <ehedlund@princeton.edu>
2017-04-28 16:57:18 -0400

    Add files via upload

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,7 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 import hello.views
+#import docs
 import django_cas_ng.views
+
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
     url(r'accounts/login/$', django_cas_ng.views.login),
@@ -12,6 +17,8 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db$', hello.views.db, name='db'),
     url(r'^about$', hello.views.about,name='about'),
+    url(r'^meet$', hello.views.meet,name='meet'),
+    #url(r'^casCGI$', hello.views.casCGI,name='casCGI')
     url(r'^status$', hello.views.status, name='status'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/$', include(admin.site.urls)),
 ]
