6cc7959840380840ad35b00d05c18851f015736a
mayaprinceton <mayap@princeton.edu>
2017-04-30 20:53:24 -0400

    Created json object to store static data
    static data from file current_stats to json file json_data

--- hello/templates/load_data.py
+++ hello/templates/load_data.py
@@ -1,12 +1,22 @@
-import sys,os
-import csv
-sys.path.append(your_djangoproject_home)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+import sys
+import json
 
-dataReader = csv.reader(open(dummy_occupancy.csv), delimiter=',', quotechar='"')
-occupancy = []
-int index
-for row in dataReader:
-  occupancy[index] = row[index]
-  index+=1
-  
+# sys.path.append(your_djangoproject_home)
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+data = {}
+dillonOcc = 0
+fristOcc = 0
+
+with open('current_stats') as f:
+	for line in f:
+		split = line.split()
+		if split[0] == "Dillon-Gym":
+			dillonOcc += int(split[2])
+		if split[0] == "Frist-Campus-Center":
+			fristOcc += int(split[2])
+	data['Dillon-Gym'] = dillonOcc
+	data['Frist-Campus'] = fristOcc
+	json_data = json.dumps(data)
+
+print 'JSON: ', json_data
