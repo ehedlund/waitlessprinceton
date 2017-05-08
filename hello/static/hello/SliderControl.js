9347c7753d7b892a7111cb7d1ffbc027086914d6
Emily <emilyhedlund3@gmail.com>
2017-05-07 22:42:31 -0400

    start slider in middle?

--- hello/static/hello/SliderControl.js
+++ hello/static/hello/SliderControl.js
@@ -92,7 +92,22 @@ L.Control.SliderControl = L.Control.extend({
     startSlider: function () {
         _options = this.options;
         _extractTimestamp = this.extractTimestamp
-        var index_start = _options.minValue;
+
+        var now = new Date();
+        var hour = now.getHours();
+        var minutes = now.getMinutes();
+        if (minutes < 15) {
+            minutes = "00";
+        } else if (minutes < 45){
+            minutes = "30";
+        } else {
+            minutes = "00";
+            ++hour;
+        }
+        var time = hour.concat(minutes);
+        var times = ["000", "030", "100", "130", "200", "230", "300", "330", "400", "430", "500", "530", "600", "630", "700", "730", "800", "830", "900", "930", "1000", "1030", "1100", "1130", "1200", "1230", "1300", "1330", "1400", "1430", "1500", "1530", "1600", "1630", "1700", "1730", "1800", "1830", "1900", "1930", "2000", "2030", "2100", "2130", "2200", "2230", "2300", "2330"];
+        var index_start = times.indexOf(time);
+        //var index_start = _options.minValue;
         if(_options.showAllOnStart){
             index_start = _options.maxValue;
             if(_options.range) _options.values = [_options.minValue,_options.maxValue];
