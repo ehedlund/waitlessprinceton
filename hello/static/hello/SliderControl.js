b3421bbae11ea4765183d08bd8cc4ba666d6f071
mayaprinceton <mayap@princeton.edu>
2017-05-08 13:36:06 -0400

    Update SliderControl.js

--- hello/static/hello/SliderControl.js
+++ hello/static/hello/SliderControl.js
@@ -6,6 +6,7 @@ L.Control.SliderControl = L.Control.extend({
         isEpoch: false,     // whether the time attribute is seconds elapsed from epoch
         startTimeIdx: 0,    // where to start looking for a timestring
         timeStrLength: 19,  // the size of  yyyy-mm-dd hh:mm:ss - if millis are present this will be larger
+//         value: 0,
         maxValue: -1,
         minValue: 0,
         showAllOnStart: false,
@@ -101,9 +102,9 @@ L.Control.SliderControl = L.Control.extend({
         $("#leaflet-slider").slider({
 //             this.options.value = startPosition;
             range: _options.range,
-            value: _options.value,
-//          values: _options.values,
-            values: startPosition,
+//          value: _options.value,
+            value: startPosition,
+            values: _options.values,
             min: _options.minValue,
             max: _options.maxValue,
             step: 1,
